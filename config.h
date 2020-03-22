5b24d2c46546d86b73c1ed6b2b44c6f628dbedfd
Ricardas Navickas <ricardasnv@hotmail.com>
2020-03-22 18:24:52 +0200

    Add comments to config.h

--- config.h
+++ config.h
@@ -1,12 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+// Control files
 #define SYSFILE "/sys/class/backlight/intel_backlight/brightness"
 #define SYSMAXFILE "/sys/class/backlight/intel_backlight/max_brightness"
 
+// Default and current brightness files
 #define DEFBRFILE "/.backlight/default"
 #define CURBRFILE "/.backlight/current"
 
+// Minimum allowable brightness
+// (best to set it at >0 so that brightness level doesn't get stuck at zero
+//  when only mul/div is used)
 #define MIN_BRIGHTNESS 0.5
 
 #endif
